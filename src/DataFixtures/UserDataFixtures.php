24f5c92f0df047ae5333f10791bac9422c304dc1
phmaeso <phmaeso@gmail>
2021-04-10 13:56:12 +0200

    creado Fixture Usuario

---
+++ src/DataFixtures/UserDataFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Usuario;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    $id=0;
+
+    public function load(ObjectManager $user)
+    {
+        //meter usuarios
+        $user = new $Usuario();
+        $user->$id++;
+        $user->setDni(70073677Z);
+        $user->setNombre('Patricia');
+        $user->setApellido('Herranz Maeso');
+        $user->setEmail('phmaeso@gmail.com');
+        $user->setPass('pass');
+
+
+        //subir usuarios
+        $user->flush();
+    }
+}
+
