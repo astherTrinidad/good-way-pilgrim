07fce64c035ca1a5492869bc657064e0cb951e74
Irene_Cloud <irene.sanchez@clouddistrict.com>
2021-04-10 18:10:01 +0200

    comenzando login y registro

--- src/Controller/authenticationController.php
+++ src/Controller/authenticationController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,9 +16,16 @@ class authenticationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        $password = $request->get('password');
+        $name = $request->get('name');
+        $surname = $request->get('surname');
+        $dni = $request->get('dni');
         $email = $request->get('email');
+        $password = $request->get('password');
         $user = new Usuario();
+        $user->setNombre($name);
+        $user->setApellido($surname);
+        $user->setDni($dni);
+        //$user->setPass($password);
         $user->setPass($encoder->encodePassword($user, $password));
         $user->setEmail($email);
         $em = $this->getDoctrine()->getManager();
@@ -31,11 +39,12 @@ class authenticationController extends AbstractController
     /**
  * @Route("/auth/login", name="login", methods={"POST"})
  */
-public function login(Request $request, UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
-{
-        $user = $userRepository->findOneBy([
-                'email'=>$request->get('email'),
-        ]);
+public function login(Request $request, UsuarioRepository $userRepository, UserPasswordEncoderInterface $encoder){
+
+        $user = $userRepository->getOneByEmail($request->get('email'));
+//        $user = $userRepository->findOneBy([
+//                'email'=>$request->get('email'),
+//        ]);
         if (!$user || !$encoder->isPasswordValid($user, $request->get('password'))) {
                 return $this->json([
                     'message' => 'email or password is wrong.',
--- src/Repository/UsuarioRepository.php
+++ src/Repository/UsuarioRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Usuario;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 /**
  * @method Usuario|null find($id, $lockMode = null, $lockVersion = null)
@@ -18,6 +19,8 @@ class UsuarioRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Usuario::class);
     }
+    
+    
 
     // /**
     //  * @return Usuario[] Returns an array of Usuario objects
@@ -35,6 +38,30 @@ class UsuarioRepository extends ServiceEntityRepository
         ;
     }
     */
+    public function getOneByEmail ($email){
+        $em = $this->getEntityManager();
+        $db = $em->getConnection();
+ 
+        $query = "SELECT * FROM usuario where email = '$email' ";
+        $resultado = $db->executeQuery($query);
+        $po=$resultado->fetchAll();
+ 
+        // Mostrar todo
+        foreach ($po as $p) {
+            echo $p["nombre"];
+            echo "<br/>";
+            echo $p["apellido"];
+            echo "<hr/>";
+        }
+
+
+        dump($resultado);
+        die();
+//        $qb = $this->getEntityManager()->createQueryBuilder();
+//        $qb->select('u')->from(Usuario::class, 'u')->where('u.email = :email')->setParameter('email', $email);
+//        $user = $qb->getQuery()->getResult();
+//        return $user[0];
+    }
 
     /*
     public function findOneBySomeField($value): ?Usuario
