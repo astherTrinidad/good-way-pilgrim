d85aa9ad5d55f7195de2fdf470d98a741664a68a
astherTrinidad <asther.tmora@gmail.com>
2021-04-23 13:14:47 +0200

    cambios prueba

--- src/Controller/authenticationController.php
+++ src/Controller/authenticationController.php
@@ -14,6 +14,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Firebase\JWT\JWT;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 
 class authenticationController extends AbstractController
 {
@@ -90,7 +93,6 @@ class authenticationController extends AbstractController
     public function showProfile(Request $request, UsuarioRepository $userRepository)
     {
         $user = $userRepository->getOneById($request->get('id'));
-
         if (!$user) {
             $data = [
                 'message' => 'user not in database'
