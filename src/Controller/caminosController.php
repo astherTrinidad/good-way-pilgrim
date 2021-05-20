771afe2221381202550241adf8142a93b36d6ea4
Irene_Cloud <irene.sanchez@clouddistrict.com>
2021-05-20 10:55:21 +0200

    corregido error si no tienes camino activo

--- src/Controller/caminosController.php
+++ src/Controller/caminosController.php
@@ -58,10 +58,13 @@ class caminosController extends AbstractController {
      */
     public function getActivePath(Request $request): Response {
         $id = $this->authManager->getIdFromToken($request, $this->getParameter('jwt_secret'));
-        $paths = $this->userPathManager->getActivePath($id);
-        $etapas = $this->pathsManager->getEtapas($paths['id']);
-        $paths["etapas"] = $etapas;
-        return new JsonResponse($paths);
+        $path = $this->userPathManager->getActivePath($id);
+        if (!$path) {
+            return new JsonResponse(['message' => 'User hasnt got an active path']);
+        }
+        $etapas = $this->pathsManager->getEtapas($path['id']);
+        $path["etapas"] = $etapas;
+        return new JsonResponse($path);
     }
 
     /**
